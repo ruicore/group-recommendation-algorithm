c46ed731ad8a7581850c03c0f3e0e8b9375d82f7
HeRuivio <super76rui@icloud.com>
2019-05-11 17:24:09 +0800

    2019-05-11 把 得到用户评价过的公共项目 改成 得到评价过两个物品的公共用户

--- dataset.py
+++ dataset.py
@@ -35,9 +35,9 @@ class Data(object):
             测试集中所有的用户
         te_item: set, {item1,item2...} 
             测试集中所有的物品
-        tr_user_com_items: 
-            dict, {user1:{user2:{item1,item2},}} 
-            两个用户共同评价过的物品
+        tr_item_com_users: 
+            dict, {item1:{item2:{user1,user2...},}} 
+            评价过两个物品的公共 user
         tr_average: dict,{user1:average1,user2:} 
             训练集中每个用户对所有项目的平均评分
     """
@@ -59,10 +59,10 @@ class Data(object):
         self.te_user = list()  # type: List[str]
         self.te_item = set()  # type: Set[str]
         self.tr_average = dict()  # type: Dict[str, float]
-        self.tr_user_com_items = dict()  # type: Dict[str, Dict[str, Set[str]]]
+        self.tr_item_com_users = dict()  # type: Dict[str, Dict[str, Set[str]]]
         self.build(tr_data, self.tr_dict, self.tr_user, self.tr_item)
         self.build(te_data, self.te_dict, self.te_user, self.te_item)
-        self.build_user_common_items()
+        self.build_item_common_users()
         self.build_average()
 
     def build(self, data: List[List[str]], table: Dict, user_list: List[str],
@@ -96,29 +96,24 @@ class Data(object):
             item_set.add(item)
         return
 
-    def build_user_common_items(self) -> None:
+    def build_item_common_users(self) -> None:
         """
-        构建 tr_user_com_items 表,此表用于存储两个用户共同评价过的项目索引
+        构建 tr_item_com_users 表,此表用于存储评价过两个项目的公共用户索引
         """
 
-        # 建立 item -- users 关系表
-        # item_users：{item1:{user1,user2...}...}
-        item_users = dict()  # type: Dict[str, Set[str]]
         for user, items in self.tr_dict.items():
-            for item in items.keys():
-                if item not in item_users: item_users[item] = set()
-                item_users[item].add(user)
+            for com in combinations(items.keys(), 2):
+                item_sorted = list(com)
+                item_sorted.sort()
 
-        for item, users in item_users.items():  # users 是对某一个 item 评过分的所有用户
-            for com in combinations(users, 2):  # type: Tuple[str, ...]
-                u_sorted = list(com)
-                u_sorted.sort()
-                u, c = u_sorted[0], u_sorted[1]  # 建立字典，用 u 作为外层主键，c 作为内层主键
-                if u not in self.tr_user_com_items:
-                    self.tr_user_com_items[u] = dict()
-                if c not in self.tr_user_com_items[u]:
-                    self.tr_user_com_items[u][c] = set()
-                self.tr_user_com_items[u][c].add(item)
+                # 建立字典，用 item1 作为外层主键，item2 作为内层主键
+                item1, item2 = item_sorted[0], item_sorted[1]
+                if item1 not in self.tr_item_com_users:
+                    self.tr_item_com_users[item1] = dict()
+                if item2 not in self.tr_item_com_users[item1]:
+                    self.tr_item_com_users[item1][item2] = set()
+                self.tr_item_com_users[item1][item2].add(user)
+
         return
 
     def build_average(self) -> None:
@@ -132,25 +127,25 @@ class Data(object):
             self.tr_average[user] = float(num)
         return
 
-    def get_com_items(self, user1: str, user2: str) -> List:
+    def get_com_users(self, item1: str, item2: str) -> List[str]:
         """
         返回两个用户的评价过的物品的交集  
 
         Args:
-            user1: str，用户 ID 号
-            user2: str，用户 ID 号
+            item1: str，物品 ID 号
+            item2: str，物品 ID 号
     
         Returns：
-            coms：list，用户 user1 和用户 user2 共同评价过的项目
+            coms：list，评价过 item1 和用户 item2 的公共用户
 
         Raises：
             IOError: 
         """
-        users = sorted([user1, user2])
-        u, v = users[0], users[1]
+        items = sorted([item1, item2])
+        it1, it2 = items[0], items[1]
         commons = []  # type: List[str]
-        if u in self.tr_user_com_items and v in self.tr_user_com_items[u]:
-            commons = list(self.tr_user_com_items[u][v])
+        if it1 in self.tr_item_com_users and it2 in self.tr_item_com_users[it1]:
+            commons = list(self.tr_item_com_users[it1][it2])
         return commons
 
 
